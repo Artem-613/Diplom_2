9190d893d5e349c22fb2c2e8084e10c6ad3d17ed
Artem Shorokhov <artem.shorokhov@bk.ru>
2025-12-09 15:43:12 +0400

    fix: исправлены замечания ревьюера
    - Фикстуры вынесены в conftest.py (setup_user, setup_ingredients, setup_user_teardown)
    - Вспомогательные функции вынесены в data.py (create_burger, init_teardown)
    - В тестах остались только тестовые методы
    - Все тесты используют фикстуры из conftest.py

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -4,46 +4,33 @@ import allure
 from data import StatusCodes as CODE
 from data import ResponseKeys as KEYS
 from data import ResponseMessages as message
+from data import create_burger  # ← НОВЫЙ ИМПОРТ
 
 from helpers.helpers_check_response import HelpersOnCheck as c
 from helpers.helpers_create_user import HelpersOnCreateUser as u
 from helpers.helpers_get_ingredients import HelpersOnGetIngredients as g
 
 
-@pytest.fixture(scope='class')
-@allure.title('Инициализируем списки ингредиентов')
-def setup_ingredients():
-    ingredients = g.get_ingredients()
-    TestCreateOrder.buns_list = g.get_buns_list(ingredients)
-    TestCreateOrder.fillings_list = g.get_fillings_list(ingredients)
-    TestCreateOrder.sauces_list = g.get_sauces_list(ingredients)
-    c.check_ingredients(TestCreateOrder.buns_list, TestCreateOrder.fillings_list, TestCreateOrder.sauces_list)
-
-
-@pytest.mark.usefixtures('setup_ingredients', scope='class')
+@pytest.mark.usefixtures('setup_ingredients')
 class TestCreateOrder:
 
-    buns_list = None
-    fillings_list = None
-    sauces_list = None
-
-
     @allure.step('Собираем бургер для заказа')
-    def __create_burger(self):
-        ingredients_list = [
-            (self.buns_list[0])[KEYS.ID_KEY],
-            (self.fillings_list[0])[KEYS.ID_KEY],
-            (self.sauces_list[0])[KEYS.ID_KEY],
-        ]
-        return ingredients_list
+    def get_burger_ingredients(self, setup_ingredients):
+        # Получаем ингредиенты из фикстуры
+        buns_list = setup_ingredients['buns_list']
+        fillings_list = setup_ingredients['fillings_list']
+        sauces_list = setup_ingredients['sauces_list']
+        
+        # Используем функцию из data.py
+        return create_burger(buns_list, fillings_list, sauces_list)
 
 
     @allure.title('Проверка создания заказа для авторизованного пользователя')
-    def test_create_order_authorized_user(self, setup_user):
+    def test_create_order_authorized_user(self, setup_user, setup_ingredients):
         # сохраняем авторизационный токен пользователя, полученный при регистрации
         user_data, auth_token = setup_user
         # составляем список ингредиентов для бургера
-        ingredients_id_list = self.__create_burger()
+        ingredients_id_list = self.get_burger_ingredients(setup_ingredients)
         # отправляем запрос на создание заказа
         response = u.try_to_create_order(ingredients_id_list, auth_token)
 
@@ -52,11 +39,11 @@ class TestCreateOrder:
 
 
     @allure.title('Проверка создания заказа для авторизованного пользователя')
-    def test_create_order_two_orders_for_authorized_user(self, setup_user):
+    def test_create_order_two_orders_for_authorized_user(self, setup_user, setup_ingredients):
         # сохраняем авторизационный токен пользователя, полученный при регистрации
         user_data, auth_token = setup_user
         # составляем список ингредиентов для бургера
-        ingredients_id_list = self.__create_burger()
+        ingredients_id_list = self.get_burger_ingredients(setup_ingredients)
         # отправляем запрос на создание заказа
         response = u.try_to_create_order(ingredients_id_list, auth_token)
         # проверяем полученный ответ и данные заказа
@@ -69,9 +56,9 @@ class TestCreateOrder:
 
 
     @allure.title('Проверка создания заказа без авторизации')
-    def test_create_order_unauthorized(self):
+    def test_create_order_unauthorized(self, setup_ingredients):
         # составляем список ингредиентов для бургера
-        ingredients_id_list = self.__create_burger()
+        ingredients_id_list = self.get_burger_ingredients(setup_ingredients)
         # отправляем запрос на создание заказа
         response = u.try_to_create_order(ingredients_id_list)
 
@@ -101,4 +88,3 @@ class TestCreateOrder:
 
         # проверяем что получен код ответа 500
         c.check_status_code(response, CODE.ERROR_500)
-
